42a72f5bb1eee803b0b3de4a1f8be64a70717fde
kiko <cahenriquesoares@gmail.com>
2021-06-23 20:04:59 -0300

    Criadas as docstrings das funções

--- funcoes.py
+++ funcoes.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 
 def escolha_da_palavra():
     """
-    Escolhe a palavra para ser inicializado o jogo
+    Escolhe a palavra e cria o arquivo palavra.txt na qual ela será armazenada
 
     :return: str randomizada pela API 'dicionário aberto'
     """
@@ -17,6 +17,10 @@ def escolha_da_palavra():
 
 
 def dica():
+    '''
+    Busca a definição da palavra escolhida
+    :return: str da definição da palavra até o primeiro ponto final.
+    '''
     with open('palavra.txt') as palavra:
         requisicao_palavra = requests.get(f'https://api.dicionario-aberto.net/word/{palavra.read()}')
         xml = requisicao_palavra.json()
@@ -27,6 +31,10 @@ def dica():
 
 
 def palavra_utilizada():
+    '''
+    Lê a palavra escolhida, porém trabalhada
+    :return: str da palavra sem acentos, caracteres especiais e espaços
+    '''
     with open('palavra.txt') as palavra:
         palavra_do_arquivo = palavra.read()
         palavra_retrabalhada = unidecode(palavra_do_arquivo.upper().strip().replace('-', ''))
@@ -34,5 +42,9 @@ def palavra_utilizada():
 
 
 def palavra_do_arquivo():
+    '''
+    Lê a palavra escoliha do arquivo palavra.txt
+    :return: str palavra do arquivo palavra.txt
+    '''
     with open('palavra.txt') as palavra:
         return palavra.read()
